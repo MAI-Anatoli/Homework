5925b734e9693430c003e8901ce69b5244388eb6
Anatoli Marian <bmwm5f15.tm@gmail.com>
2023-12-10 22:08:32 +0200

    Seminar02

---
+++ Seminar02/task02/Program.cs
@@ -0,0 +1,40 @@
+﻿/*Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и 
+выдаёт номер координатной четверти плоскости, в которой находится эта точка.*/
+
+
+/*Домашнее задание сделал благодаря ChatGpt сам я бы их не сделал бы не знаю что когда и зачем использовать извините за такой подход к дз.*/
+
+
+Console.WriteLine("Введите координаты X: ");
+double x = double.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите координаты Y: ");
+double y = double.Parse(Console.ReadLine());
+
+if (x != 0 && y != 0)
+{                              
+if (x > 0 && y > 0)
+{
+                                                     
+    Console.WriteLine("I квадрант");
+}
+else if (x < 0 && y > 0)
+{
+                                                       
+    Console.WriteLine("II квадрант");
+}
+else if (x < 0 && y < 0)
+{
+    
+    Console.WriteLine("III квадрант");
+}
+else if (x > 0 && y < 0)
+{
+    
+    Console.WriteLine("IV квадрант");
+}
+else if (x == 0 && y == 0)
+{
+    
+    Console.WriteLine("Начало координат");
+}
+}
---
+++ Seminar02/task4/Program.cs
@@ -0,0 +1,41 @@
+﻿/*Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.*/
+
+
+
+/*Домашнее задание сделал благодаря ChatGpt сам я бы их не сделал бы не знаю что когда и зачем использовать извините за такой подход к дз.*/
+
+   
+        Console.WriteLine("Введите натуральное число N:");
+
+        
+        string userInput = Console.ReadLine();
+
+        
+        if (int.TryParse(userInput, out int number) && number > 0)
+        {
+           
+            Console.Write("Цифры числа через запятую: ");
+            DisplayDigitsSeparatedByComma(number);
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите натуральное число.");
+        }
+    
+
+    
+    static void DisplayDigitsSeparatedByComma(int num)
+    {
+        string digitsSeparatedByComma = "";
+
+        
+        while (num > 0)
+        {
+            int digit = num % 10;  
+            digitsSeparatedByComma = digit + ", " + digitsSeparatedByComma;  
+            num /= 10;  
+        }
+
+        Console.WriteLine(digitsSeparatedByComma.TrimEnd(',', ' '));  
+    }
+
