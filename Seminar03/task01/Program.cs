7c75d60cc68a66e162d518e95db704e9db7f76d1
Anatoli Marian <bmwm5f15.tm@gmail.com>
2023-12-17 08:02:21 +0200

    Пробовал сделать ДЗ

--- Seminar03/task01/Program.cs
+++ Seminar03/task01/Program.cs
@@ -3,5 +3,53 @@
 Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].*/
 
 
-int[] array = {15, 22, 37, 41, 53, 69, 73, 86, 90, 10};
+int[] elements = {15, 20, 37, 41, 53, 69, 73, 86, 90, 100};
 
+/* void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int possition = 0;
+    while (possition < count)
+    {
+        System.Console.WriteLine(array[possition]);
+        possition++;
+    }
+}*/
+
+//int[] elements = new int[10];
+
+/*void FillArray(int[] collection)            
+                                            
+{
+    int length = collection.Length;         
+    int index = 0;                          
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);       
+        index++;
+    }
+}*/
+
+void FindingElements(int[] elements)
+{
+
+    int number = 0;
+    for (int i = 0; i < elements.Length; i++)
+    {
+        if (90 > elements[i])
+        {
+            if(elements[i] > 20)
+            {
+                number++;
+            }
+            Console.WriteLine();
+            Console.WriteLine(elements[i] + " ");
+        }
+
+    }
+    Console.WriteLine();
+    Console.WriteLine(number);
+}
+//PrintArray(elements);
+//FillArray(elements);
+FindingElements(elements);
--- Seminar03/task02/Program.cs
+++ Seminar03/task02/Program.cs
@@ -2,6 +2,55 @@
 
  Задайте массив на 10 целых чисел. Напишите программу, которая определяет количество чётных чисел в массиве.*/
 
+//int[] array = {15, 20, 37, 41, 53, 69, 73, 86, 90, 100};
+
+void FillArray(int[] col)            
+                                            
+{
+    int length = col.Length;         
+    int index = 0;                          
+    while (index < length)
+    {
+        col[index] = new Random().Next(1, 100);       
+        index++;
+    }
+}
 
 
- 
+void PrintArray(int[] arr)            
+{
+    int count = arr.Length;          
+
+    for (int i = 0; i < count; i++)     
+    {
+        Console.Write($"{arr[i]} ");  
+    }
+    Console.WriteLine();                
+}
+
+
+
+
+
+
+/*void EvenNumbers (int[] even)
+{
+   
+    for (int i = 0; i < array.Length; i++)
+    {
+         int count = 0;
+        if (array[i] % 2 == 0)
+        {
+            
+            count++;
+          
+        }
+        Console.WriteLine(count);
+    }
+}*/
+
+int[] array = new int[10];
+
+//EvenNumbers(array);
+PrintArray(array);
+FillArray(array);
